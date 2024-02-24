1b6cb1a269e45ffe5112939a0c2aab81e631a6a3
bootli <106524425+bootli@users.noreply.github.com>
2024-02-25 01:51:43 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -545,6 +545,18 @@ sudo chmod +x "${DEFAULT_PATH}"
 sed -i '/exit 0$/d' "${DEFAULT_PATH}"
 sed -i "s?112233?${SOURCE} - ${LUCI_EDITION}?g" "${DEFAULT_PATH}" > /dev/null 2>&1
 sed -i 's/root:.*/root::0:0:99999:7:::/g' ${FILES_PATH}/etc/shadow
+#添加
+    find . -type d -name 'luci-theme-argon' -o -name 'luci-theme-argonv3' -o -name 'ddns-go' -o -name 'luci-app-ddns-go' -o -name 'luci-theme-argon-mod' -o -name 'luci-app-argon-config' -o -name 'luci-theme-argone-mod' -o -name 'luci-app-argone-config' -o -name 'luci-app-v2ray-server' -o -name 'luci-app-netdata' -o -name 'autosamba' -o -name 'luci-app-samba4' -o -name 'luci-app-ikoolproxy' -o -name 'aliyundrive-webdav' | xargs -i rm -rf {}
+#find . -type d -name "r8168" -o -name "r8101" -o -name "r8125" |grep 'danshui' |xargs -i rm -rf {}
+    git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon "${HOME_PATH}/package/lean/luci-theme-argon"
+    git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config "${HOME_PATH}/package/lean/luci-app-argon-config"
+    git clone https://github.com/bootli/luci-app-v2ray-server "${HOME_PATH}/feeds/luci/applications/luci-app-v2ray-server"
+    git clone https://github.com/bootli/luci-app-samba4 "${HOME_PATH}/feeds/luci/applications/luci-app-samba4"
+    git clone https://github.com/bootli/libuild "${HOME_PATH}/package/libuild"
+    git clone https://github.com/sirpdboy/luci-app-ddns-go "${HOME_PATH}/package/ddns-go"
+    git clone https://github.com/messense/aliyundrive-webdav "${HOME_PATH}/package/li"
+
+
 if [[ `grep -Eoc "admin:.*" ${FILES_PATH}/etc/shadow` -eq '1' ]]; then
   sed -i 's/admin:.*/admin::0:0:99999:7:::/g' ${FILES_PATH}/etc/shadow
 fi
