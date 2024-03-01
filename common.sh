531acd5f0ae113b49634baa0923b3ad838450a98
bootli <106524425+bootli@users.noreply.github.com>
2024-03-01 20:47:08 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -533,13 +533,13 @@ TIME r ""
 }
 
 
-function Diy_COOLSNOWWOLF() {
+#function Diy_COOLSNOWWOLF() {
 #cd ${HOME_PATH}
 # 降低aliyundrive-webdav版本,新版本编译不成功
 #if [[ -f "${HOME_PATH}/feeds/packages/multimedia/aliyundrive-webdav/Makefile" ]]; then
   #curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/packages/aea60b5432fad984c0a4013bad0f0c5e00dcd115/multimedia/aliyundrive-webdav/Makefile  -o ${HOME_PATH}/feeds/packages/multimedia/aliyundrive-webdav/Makefile 
-fi
-}
+#fi
+#}
 
 
 function Diy_LIENOL() {
