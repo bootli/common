c2b3cbfdf8a21ea11356c44c5ff827ab68de4aa3
bootli <106524425+bootli@users.noreply.github.com>
2025-01-01 19:38:01 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -789,7 +789,7 @@ if [[ "${Customized_Information}" == "0" ]] || [[ -z "${Customized_Information}"
   echo "不进行,个性签名设置"
 elif [[ -n "${Customized_Information}" ]]; then
   sed -i "s?DESCRIPTION=.*?DESCRIPTION='OpenWrt '\" >> /etc/openwrt_release?g" "${ZZZ_PATH}"
-  sed -i "s?OpenWrt ?${Customized_Information} OpenWrt Li [2024] Compiled by Li OpenWrt ?g" "${ZZZ_PATH}"
+  sed -i "s?OpenWrt ?${Customized_Information} OpenWrt Li [2025] Compiled by Li OpenWrt ?g" "${ZZZ_PATH}"
   echo "个性签名[${Customized_Information}]增加完成"
 fi
 
